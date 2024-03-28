4dde579e44c77510344a6dec9c022ac427035a76
mingokrb <82242100+mingokrb@users.noreply.github.com>
2024-03-28 02:14:58 -0300

    Update FlashingState.hx

--- source/states/FlashingState.hx
+++ source/states/FlashingState.hx
@@ -19,12 +19,12 @@ class FlashingState extends MusicBeatState
 		super.create();
 
 		bg = new FlxBackdrop(Paths.getSharedPath('backdrop.png')); //new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
-		bg.alpha = 0.3;
+		bg.alpha = 0.2;
 		bg.updateHitbox();
 		bg.screenCenter(X);
 		add(bg);
 
-		ni = new Alphabet(0, 85, 'Cuidado!', true);
+		ni = new Alphabet(0, 90, 'Cuidado!', true);
 		ni.screenCenter(X);
 		add(ni);
 		var guh:String = 'Este mod contém algumas luzes piscantes!\n
@@ -33,13 +33,13 @@ class FlashingState extends MusicBeatState
 		Você foi avisado(a)!';
 
 		controls.isInSubstate = false; // qhar I hate it
-		warnText = new FlxText(0, FlxG.height * 0.5, FlxG.width, guh, 16);
+		warnText = new FlxText(0, FlxG.height * 0.65, FlxG.width, guh, 16);
 		warnText.setFormat(Paths.font('comicsans.ttf'), 32, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		//warnText.updateHitbox();
 		warnText.screenCenter(X);
 		add(warnText);
 
-		FlxG.sound.playMusic(Paths.music('offsetSong'), 0.5);
+		//FlxG.sound.playMusic(Paths.music('offsetSong'), 0.7);
 		addVirtualPad('NONE', 'A_B');
 	}
 
@@ -61,20 +61,20 @@ class FlashingState extends MusicBeatState
 				if(!back) {
 					ClientPrefs.data.flashing = false;
 					ClientPrefs.saveSettings();
-					FlxG.sound.music.stop();
+					//FlxG.sound.music.stop();
 					FlxG.sound.play(Paths.sound('confirmMenu'));
-					FlxTween.tween(bg, {alpha: 0}, 0.7);
-					FlxTween.tween(ni, {alpha: 0}, 0.8);
+					FlxTween.tween(bg, {alpha: 0}, 1);
+					FlxTween.tween(ni, {alpha: 0}, 1.1);
 					FlxFlicker.flicker(warnText, 1, 0.1, false, true, function(flk:FlxFlicker) {
 						new FlxTimer().start(0.2, function (tmr:FlxTimer) {
 							MusicBeatState.switchState(new TitleState());
 						});
 					});
 				} else {
-					FlxG.sound.music.stop();
+					//FlxG.sound.music.stop();
 					FlxG.sound.play(Paths.sound('cancelMenu'));
-					FlxTween.tween(bg, {alpha: 0}, 0.7);
-					FlxTween.tween(ni, {alpha: 0}, 0.8);
+					FlxTween.tween(bg, {alpha: 0}, 0.65);
+					FlxTween.tween(ni, {alpha: 0}, 0.7);
 					FlxTween.tween(warnText, {alpha: 0}, 0.6, {
 						ease: FlxEase.quadOut,
 						onComplete: function (twn:FlxTween) {
